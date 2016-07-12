88d2e0aa9325347033aa4540605bce3484e2fe2e
Maxence POUTORD <maxence.poutord@gmail.com>
2016-07-12 17:58:22 +0200

    Step 6 - Routing

--- app/hero-detail.component.ts
+++ app/hero-detail.component.ts
@@ -1,4 +1,6 @@
-import { Component, Input } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HeroService } from './hero.service';
 import { Hero } from './hero';
 
 @Component({
@@ -7,6 +9,27 @@ import { Hero } from './hero';
 })
 
 export class HeroDetailComponent {
-  @Input()
   hero: Hero;
+  sub: any;
+
+  constructor(
+    private heroService: HeroService,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit() {
+    this.sub = this.route.params.subscribe(params => {
+      let id = +params['id'];
+      this.heroService.getHero(id)
+        .then(hero => this.hero = hero);
+    });
+  }
+
+  ngOnDestroy() {
+    this.sub.unsubscribe();
+  }
+
+  goBack() {
+    window.history.back();
+  }
 }
